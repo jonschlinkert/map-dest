a4d6f5c678f76b102adef1405981e8bd02410669
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-02 04:35:31 -0400

    update tests for returning an array

--- test.js
+++ test.js
@@ -20,15 +20,29 @@ describe('errors', function () {
 });
 
 describe('mapDest', function () {
-  describe('no dest', function () {
-    it('should create a dest when no `dest` argument is passed:', function () {
-      var actual = mapDest('a/b/c.txt');
-      assert(actual.src === 'a/b/c.txt');
-      assert(actual.dest === 'a/b/c.txt');
+  describe('src', function () {
+    it('should work when src is an array:', function () {
+      var actual = mapDest('a.txt', 'dist');
+      assert(actual[0].src === 'a.txt');
+      assert(actual[0].dest === 'dist/a.txt');
+    });
+
+    it('should work when src is an array:', function () {
+      var actual = mapDest(['a.txt', 'b.txt'], 'dist');
+      assert(actual[0].src === 'a.txt');
+      assert(actual[0].dest === 'dist/a.txt');
+      assert(actual[1].src === 'b.txt');
+      assert(actual[1].dest === 'dist/b.txt');
     });
   });
 
   describe('no dest', function () {
+    it('should create a dest when no `dest` argument is passed:', function () {
+      var actual = mapDest('a/b/c.txt');
+      assert(actual[0].src === 'a/b/c.txt');
+      assert(actual[0].dest === 'a/b/c.txt');
+    });
+
     it('should work when src is an array:', function () {
       var actual = mapDest(['a.txt', 'b.txt']);
       assert(actual[0].src === 'a.txt');
@@ -41,44 +55,44 @@ describe('options', function () {
   describe('options.flatten', function () {
     it('should flatten dest when no `dest` argument is passed:', function () {
       var actual = mapDest('a/b/c.txt', {flatten: true});
-      assert(actual.src === 'a/b/c.txt');
-      assert(actual.dest === 'c.txt');
+      assert(actual[0].src === 'a/b/c.txt');
+      assert(actual[0].dest === 'c.txt');
     });
   });
 
   describe('options.ext', function () {
     it('should replace the destination extension with given ext:', function () {
       var actual = mapDest('a/b/c.txt', {ext: '.foo'});
-      assert(actual.src === 'a/b/c.txt');
-      assert(actual.dest === 'a/b/c.foo');
+      assert(actual[0].src === 'a/b/c.txt');
+      assert(actual[0].dest === 'a/b/c.foo');
     });
   });
 
   describe('options.cwd', function () {
     it('should prepend cwd to src:', function () {
       var actual = mapDest('a/b/c.txt', {cwd: 'one/two'});
-      assert(actual.src === 'one/two/a/b/c.txt');
-      assert(actual.dest === 'a/b/c.txt');
+      assert(actual[0].src === 'one/two/a/b/c.txt');
+      assert(actual[0].dest === 'a/b/c.txt');
     });
 
     it('should prepend cwd to src and flatten dest:', function () {
       var actual = mapDest('a/b/c.txt', {cwd: 'one/two', flatten: true});
-      assert(actual.src === 'one/two/a/b/c.txt');
-      assert(actual.dest === 'c.txt');
+      assert(actual[0].src === 'one/two/a/b/c.txt');
+      assert(actual[0].dest === 'c.txt');
     });
   });
 
   describe('options.destBase', function () {
     it('should prepend destBase to generated dest:', function () {
       var actual = mapDest('a/b/c.txt', {destBase: 'one/two'});
-      assert(actual.src === 'a/b/c.txt');
-      assert(actual.dest === 'one/two/a/b/c.txt');
+      assert(actual[0].src === 'a/b/c.txt');
+      assert(actual[0].dest === 'one/two/a/b/c.txt');
     });
 
     it('should prepend destBase to dest:', function () {
       var actual = mapDest('a/b/c.txt', 'foo', {destBase: 'one/two'});
-      assert(actual.src === 'a/b/c.txt');
-      assert(actual.dest === 'one/two/foo/a/b/c.txt');
+      assert(actual[0].src === 'a/b/c.txt');
+      assert(actual[0].dest === 'one/two/foo/a/b/c.txt');
     });
   });
 
@@ -89,7 +103,7 @@ describe('options', function () {
           return src;
         }
       });
-      assert(actual.dest === 'a/b/c.md');
+      assert(actual[0].dest === 'a/b/c.md');
     });
 
     it('should expose target properties as `this` to rename function:', function () {
@@ -98,7 +112,7 @@ describe('options', function () {
           return path.join(path.dirname(dest), 'blog', path.basename(fp));
         }
       });
-      actual.dest.should.equal('foo/blog/index.js');
+      actual[0].dest.should.equal('foo/blog/index.js');
     });
   });
 });
