d857c5a99b650c2dadbd03a963690d580f5671d0
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-04 08:40:39 -0400

    docs

--- example.js
+++ example.js
@@ -1,4 +1,19 @@
 var mapDest = require('./');
-console.log(mapDest('d.md', {cwd: 'a/b/c'}));
-console.log(mapDest('d.md', {cwd: 'a/b/c', ext: '.html'}));
-console.log(mapDest('templates/base.hbs', {cwd: '@/boilerplate-h5bp'}));
+console.log(mapDest('d.md', {
+  cwd: 'a/b/c'
+}));
+console.log(mapDest('d.md', {
+  cwd: 'a/b/c',
+  ext: '.html'
+}));
+console.log(mapDest('templates/base.hbs', {
+  cwd: '@/boilerplate-h5bp'
+}));
+console.log(mapDest('scaffolds', {
+  cwd: '~'
+}));
+
+console.log(mapDest('.', {
+  srcBase: 'scaffolds',
+  cwd: '~'
+}));
